7c517b993c68cc8c903c6c952793cb2834cb5bae
Rachel <rachbohm@gmail.com>
2023-03-15 23:53:38 -0500

    added firstName lastName

--- backend/db/seeders/20230316032804-demo-user.js
+++ backend/db/seeders/20230316032804-demo-user.js
@@ -13,17 +13,23 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: "Arnold",
+        lastName: "Schwartzenegger"
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstName: "Ronnie",
+        lastName: "Coleman"
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3'),
+        firstName: "Dwayne",
+        lastName: "TheRockJohnson"
       }
     ], {});
   },
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -18,6 +18,8 @@ router.get(
         id: user.id,
         email: user.email,
         username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName
       };
       return res.json({
         user: safeUser
@@ -64,6 +66,8 @@ router.post(
       id: user.id,
       email: user.email,
       username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName
     };
 
     await setTokenCookie(res, safeUser);
