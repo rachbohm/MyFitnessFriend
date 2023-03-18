877f17b3fc70ca126013ca7c17f2ad8a0e4cdfd6
Rachel <rachbohm@gmail.com>
2023-03-18 17:47:12 -0500

    hidden details

--- frontend/src/components/MyFoodPage/MyFoodList.js
+++ frontend/src/components/MyFoodPage/MyFoodList.js
@@ -1,31 +1,61 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { loadMyFoodsThunk } from '../../store/foods';
+import './MyFoodList.css';
 
 const MyFoodList = () => {
   const dispatch = useDispatch();
+  const [showDetails, setShowDetails] = useState(false);
+  const ulRef = useRef();
+
+  const openDetails = () => {
+    if (showDetails) return;
+    setShowDetails(true);
+  }
 
   useEffect(() => {
     dispatch(loadMyFoodsThunk())
   }, [dispatch]);
 
-  let foods = useSelector(state => state.foodState);
-  let foodsArr = Object.values(foods);
+useEffect(() => {
+  if (!showDetails) return;
 
-  return (
-    <ul>My Foods
-      {foodsArr.map((food) => (
-        <li key={food.id}>Name: {food.foodName}
+  const closeDetails = (e) => {
+    if (!ulRef.current.contains(e.target)) {
+      setShowDetails(false);
+    }
+  };
+
+  document.addEventListener('click', closeDetails);
+
+  return () => document.removeEventListener("click", closeDetails);
+}, [showDetails]);
+
+
+let foods = useSelector(state => state.foodState);
+let foodsArr = Object.values(foods);
+
+
+const ulClassName = "food-details" + (showDetails ? "" : " hidden");
+
+return (
+  <ul>
+    <h2>Your Personal Foods</h2>
+    {foodsArr.map((food) => (
+      <li key={food.id}><button onClick={openDetails}>{food.foodName}</button>
+        <ul className={ulClassName} ref={ulRef}>
+
           <div>Calories: {food.calories}</div>
           <div>Carbohydrates(grams): {food.carbohydrates}</div>
           <div>Fat(grams): {food.fat}</div>
           <div>Protein(grams): {food.protein}</div>
           <div>Serving Size: {food.servingSizeNum} {food.servingSizeUnit}</div>
           <div>Servings Per Container: {food.servingsPerContainer}</div>
-        </li>
-      ))}
-    </ul>
-  )
+        </ul>
+      </li>
+    ))}
+  </ul>
+)
 }
 
 export default MyFoodList;
